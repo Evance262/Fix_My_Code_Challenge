66d97927aae38898182266c79e35f9583eff5ede
Evans262 <evancechapuma62@gmail.com>
2022-02-12 20:02:53 +0200

    fixed

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -54,7 +54,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
@@ -87,7 +87,6 @@ if __name__ == '__main__':
     if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
-
 
     if user_1.is_valid_password("Fakepwd"):
         print("is_valid_password should return False if it's not the right \
